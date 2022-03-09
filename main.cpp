c093003ab01ecf4574861d5c445012623be2eb95
ppparamonov <ppparamonov@sberbank.ru>
2022-03-09 21:16:41 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -3,14 +3,18 @@
 #include <cstring>
 #include <PikaCompiler.h>
 
-int main() {
+int main(int argc, char** argv) {
+  if (argc < 3) {
+    std::cout << "usage: <in> <out>" << std::endl;
+    std::terminate();
+  }
   PikaCompiler compiler;
-  compiler.SetSource("../factorial");
-  compiler.SetExecutable("../output");
+  compiler.SetSource(argv[1]);
+  compiler.SetExecutable(argv[2]);
   compiler.Compile();
   compiler.WriteBinary();
   PikaExecutor executor;
-  executor.ReadBinary("../output");
+  executor.ReadBinary(argv[2]);
   executor.Execute();
   return 0;
 }
